349564c45d172421d0de252d689d91f377f1ffd5
Evert Pot <evertpot@gmail.com>
2010-11-28 19:41:51 +0100

    Added caldav-proxy document

---
+++ tests/Sabre/DAV/Auth/PrincipalCollectionTest.php
@@ -0,0 +1,92 @@
+<?php
+
+require_once 'Sabre/DAV/Auth/MockBackend.php';
+
+class Sabre_DAV_Auth_PrincipalCollectionTest extends PHPUnit_Framework_TestCase {
+
+    public function testBasic() {
+
+        $backend = new Sabre_DAV_Auth_MockBackend();
+        $pc = new Sabre_DAV_Auth_PrincipalCollection($backend);
+        $this->assertTrue($pc instanceof Sabre_DAV_Auth_PrincipalCollection);
+
+        $this->assertEquals('principals',$pc->getName());
+
+    }
+
+    /**
+     * @depends testBasic
+     */
+    public function testGetChildren() {
+
+        $backend = new Sabre_DAV_Auth_MockBackend();
+        $pc = new Sabre_DAV_Auth_PrincipalCollection($backend);
+        
+        $children = $pc->getChildren();
+        $this->assertTrue(is_array($children));
+
+        foreach($children as $child) {
+            $this->assertTrue($child instanceof Sabre_DAV_Auth_Principal);
+        }
+
+    }
+
+    /**
+     * @depends testBasic
+     * @expectedException Sabre_DAV_Exception_MethodNotAllowed
+     */
+    public function testGetChildrenRestricted() {
+
+        $backend = new Sabre_DAV_Auth_MockBackend();
+        $pc = new Sabre_DAV_Auth_PrincipalCollection($backend);
+        $pc->disallowListing = true;
+        
+        $children = $pc->getChildren();
+
+    }
+
+    /**
+     * @depends testBasic
+     */
+    public function testGetChildRestrictedSelf() {
+
+        $backend = new Sabre_DAV_Auth_MockBackend();
+        $backend->setCurrentUser('principals/admin');
+        $pc = new Sabre_DAV_Auth_PrincipalCollection($backend);
+        $pc->disallowListing = true;
+        
+        $child = $pc->getChild('admin');
+        $this->assertTrue($child instanceof Sabre_DAV_Auth_Principal);
+
+    }
+
+
+    /**
+     * @depends testBasic
+     * @expectedException Sabre_DAV_Exception_Forbidden
+     */
+    public function testGetChildRestrictedOtherUser() {
+
+        $backend = new Sabre_DAV_Auth_MockBackend();
+        $backend->setCurrentUser('principals/admin');
+        $pc = new Sabre_DAV_Auth_PrincipalCollection($backend);
+        $pc->disallowListing = true;
+        
+        $child = $pc->getChild('user1');
+
+    }
+
+    /**
+     * @depends testBasic
+     * @expectedException Sabre_DAV_Exception_Forbidden
+     */
+    public function testGetChildRestrictedNotLoggedIn() {
+
+        $backend = new Sabre_DAV_Auth_MockBackend();
+        $pc = new Sabre_DAV_Auth_PrincipalCollection($backend);
+        $pc->disallowListing = true;
+        
+        $child = $pc->getChild('user1');
+
+    }
+}
