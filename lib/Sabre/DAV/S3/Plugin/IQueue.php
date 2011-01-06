10b6f951685f0a556d211e67dfe47d9e1f41aa6a
Paul Voegler <Paul Voegler@localhost>
2011-01-06 08:26:42 +0100

    Add write locks to shared queue

--- lib/Sabre/DAV/S3/Plugin/IQueue.php
+++ lib/Sabre/DAV/S3/Plugin/IQueue.php
@@ -10,82 +10,101 @@
  */
 interface Sabre_DAV_S3_Plugin_IQueue
 {
-	/**
-	 * Try to acquire a lock for processing power
-	 *
-	 * @param int maxlocks
-	 * @return int|bool the lock number or false if no lock is available
-	 */
-	public function acquireLock();
+	const LOCK_READ = 1;
+
+	const LOCK_WRITE = 2;
 
 	/**
-	 * Release the lock for processing power
+	 * Try to acquire a read or write lock
 	 *
-	 * @param $lock
-	 * @return void
+	 * @param string $locktype [LOCK_READ, LOCK_WRITE]
+	 * @return integer|boolean the lock number or false if no lock is available
+	 */
+	public function acquireLock($locktype);
+
+	/**
+	 * Release the read or write lock
+	 *
+	 * @param integer $lock
+	 * @return boolean
 	 */
 	public function releaseLock($lock);
 
 	/**
 	 * Checks if the queue is empty
 	 *
-	 * @return bool
+	 * @param integer $lock a valid read or write lock
+	 * @return boolean
 	 */
-	public function isEmpty();
+	public function isEmpty($lock = 0);
 
 	/**
 	 * Add data to the end of the queue
+	 * May be an array to write multiple records. Pass an array of an array to write an array.
 	 *
-	 * @param $data mixed|array
-	 * @return bool
+	 * @param mixed $data
+	 * @param integer $lock a valid write lock
+	 * @return boolean
 	 */
-	public function enqueue($data);
+	public function enqueue($data, $lock = 0);
 
 	/**
 	 * Add data to the end of the queue
+	 * May be an array to write multiple records. Pass an array of an array to write an array.
 	 *
-	 * @param $data mixed|array
-	 * @return bool
+	 * @param mixed $data
+	 * @param integer $lock a valid write lock
+	 * @return boolean
 	 */
-	public function push($data);
+	public function push($data, $lock = 0);
 
 	/**
 	 * Get the top entry and delete it from the queue
 	 *
-	 * @param $lock a valid lock
+	 * @param $lock a valid read lock
 	 * @return mixed The first queue entry or false on failure
 	 */
-	public function dequeue($lock);
+	public function dequeue($lock = 0);
 
 	/**
 	 * Get the top entry and delete it from the queue
 	 *
-	 * @param $lock a valid lock
+	 * @param $lock a valid read lock
 	 * @return mixed The first queue entry or false on failure
 	 */
-	public function shift($lock);
+	public function shift($lock = 0);
 
 	/**
 	 * Get the last entry and delete it from the queue
 	 *
-	 * @param $lock a valid lock
+	 * @param $lock a valid read lock
 	 * @return mixed The last queue entry or false on failure
 	 */
-	public function pop($lock);
+	public function pop($lock = 0);
+
+	/**
+	 * Add data to the top of the queue
+	 * May be an array to write multiple records. Pass an array of an array to write an array.
+	 *
+	 * @param mixed $data
+	 * @param integer $lock a valid write lock
+	 * @return boolean
+	 */
+	public function unshift($data, $lock = 0);
 
 	/**
 	 * reorganize the queue file
 	 *
-	 * @param $lock a valid lock
+	 * @param $lock a valid write lock
 	 * @return bool
 	 */
-	public function reorganize($lock);
+	public function reorganize($lock = 0);
 
 	/**
 	 * clear the queue file
 	 *
-	 * @param $lock a valid lock
+	 * @param $lock a valid write lock
 	 * @return bool
 	 */
-	public function clear($lock);
+	public function clear($lock = 0);
 }
